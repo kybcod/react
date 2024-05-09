279398688fd9d0135480eb7d4684649a8714e9f7
kyb <kyb-0215@naver.com>
2024-05-09 14:42:36 +0900

    자바 스크립트 데이터 타입 undefined, string, number, boolean, object function etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
 function App() {
-  return null;
+  // javascript review
+
+  //javascript data type
+  let a = undefined; //undefined : 값을 주지 않을 때
+  console.log("a", a);
+  let b = "문자열 타입";
+  console.log("b", b);
+  let c = 3;
+  console.log("c", typeof c);
+  let d = 3.14;
+  console.log("d", typeof d);
+  let e = true;
+  console.log("e", typeof e);
+
+  // 객체 타입
+  let f = {};
+  console.log("f", typeof f);
+
+  let g = function () {}; // 함수는 function 타입
+  console.log("g", typeof g);
+  g = {};
+  console.log("g", typeof g);
+
+  return <div></div>;
 }
 export default App;
