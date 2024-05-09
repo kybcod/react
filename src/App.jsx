9d350c10a8b48387263a72ef2a794e885245bed1
kyb <kyb-0215@naver.com>
2024-05-09 11:38:22 +0900

    리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,5 @@
-function MyComponent() {
-  //최상위 컴포넌트 사용하지 않을 떄 -> fragment <></> 태그명이 없는 태그 사용
-  return (
-    <>
-      <p>Lorem ipsum dolor.</p>
-      <p>Commodi, iusto, vitae?</p>
-    </>
-  );
-}
-
-function MyComp() {
-  // 최상위 컴포너는 1개
-  return (
-    <div>
-      <p>Lorem ipsum.</p>
-      <p>Incidunt, porro?</p>
-    </div>
-  );
-}
-
-function MyTag() {
-  // jsx 코드가 여러 줄일 때 () 사용
-  return (
-    <div>
-      hello tag
-      <p>Lorem ipsum.</p>
-      <p>At, voluptates.</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <div>
-      <MyTag />
-      <MyComp />
-      <MyComponent />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
