e39b2e50d5d399895e6dadcb438adaa17beb1fd3
kyb <kyb-0215@naver.com>
2024-05-09 14:32:13 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
 function App() {
-  // &&, ||
-  // falsy(false 같은) : false, 0, "", null, undefined, NaN
-
-  const a = undefined;
-  if (a) {
-    console.log("a는 참이다.");
-  } else {
-    console.log("a는 거짓이다.");
-  }
-
-  // truthy(true 같은) : falsy가 아니면 truthy
-
-  // &&, ||와 조합
-  let b = 0;
-  let c = b && "값이 있음";
-  console.log("c", c); //0
-
-  let d = b || "값이 없음";
-  console.log("d", d);
-
-  return <div></div>;
+  return null;
 }
-
 export default App;
