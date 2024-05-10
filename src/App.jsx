81059f79f03c527bd8c0011c9a5e9817c8298106
kyb <kyb-0215@naver.com>
2024-05-10 16:31:40 +0900

    style props 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return <button style={sx}>{name}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton
+        name="클릭1"
+        sx={{
+          borderColor: "blue",
+          backgroundColor: "skyblue",
+          padding: "20px",
+          color: "blue",
+        }}
+      />
+      <MyButton
+        name="클릭2"
+        sx={{
+          borderColor: "red",
+          backgroundColor: "plum",
+          padding: "20px",
+          color: "red",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
