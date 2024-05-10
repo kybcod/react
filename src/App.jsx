c77620d71ad6793108e9058e0aacc27b91e72b36
kyb <kyb-0215@naver.com>
2024-05-10 15:11:12 +0900

    props의 값타입은 javascript 자료형, 값은 {}안에 작성, 단, string 생략 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  {
+    /*props의 값 타입은 javascript 값타입*/
+  }
+  {
+    /*{}에 값 입력, (string 만 생략 가능)*/
+  }
+  return (
+    <div>
+      <MyComp
+        name={"son"}
+        street="gangnam"
+        age={3.14}
+        address={{ city: "seoul", country: "us" }}
+        married={true}
+        action={function () {
+          console.log("action");
+        }}
+        someAction={() => console.log("someAction")}
+      />
+    </div>
+  );
 }
 
 export default App;
