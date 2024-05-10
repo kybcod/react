80c3a4b5d91cae2d02c1af65907f94c53ef34a89
kyb <kyb-0215@naver.com>
2024-05-10 15:19:56 +0900

    props의 명명 규칙 lowerCamelCase, 예약어, class -> className, label for -> htmlFor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  //props의 이름은 lowerCamelCase로 작성
+  // 예약어를 사용하지 말아야 합니다.
+  return (
+    <div>
+      <label htmlFor="input1"></label>
+      <input id="input1" type="text" />
+      <p className="error note">Lorem ipsum dolor.</p>
+      <MyComp number={3} pageNumber={3} />
+    </div>
+  );
 }
 
 export default App;
