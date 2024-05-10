84df81a6f5474a09aafd8319c5fb8d64d7376373
kyb <kyb-0215@naver.com>
2024-05-10 09:23:48 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // string
-  const s1 = "some string";
-  const s2 = "other string";
-  const s3 = " I'm 신뢰에요";
-  const s4 = '제 이름은 "흥민"입니다.';
-  const s5 = `backtick string`; //template literal
-
-  const name = "son";
-  const age = 30;
-  const s6 = "son" + "is " + age + " years.";
-  const s7 = `${name} is ${age} years.`;
-
-  return (
-    <div>
-      {s6} {s7}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
