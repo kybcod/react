4ef648ee0f196139c9303384b3874fddfa9c4bc4
kyb <kyb-0215@naver.com>
2024-05-13 14:05:10 +0900

    버튼 클릭 시 1씩 증가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React, { useState } from "react";
 
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-  const [val2, setVal2] = useState(0);
-
-  console.log("다시 그려짐");
-
+function MyCounter() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <div>{val1}</div>
       <div>
-        <button onClick={() => setVal1(val1 + 1)}>click1</button>
-      </div>
-      <div>{val2}</div>
-      <div>
-        <button onClick={() => setVal2(val2 + 1)}>click2</button>
+        {count}
+        <button onClick={() => setCount(count + 1)}>증가</button>
       </div>
     </div>
   );
@@ -23,7 +15,7 @@ function MyComp() {
 function App(props) {
   return (
     <div>
-      <MyComp />
+      <MyCounter />
     </div>
   );
 }
