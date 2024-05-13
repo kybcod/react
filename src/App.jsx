3b2a89a593d6d8a8c1832e21ab7f23291f746458
kyb <kyb-0215@naver.com>
2024-05-13 10:26:36 +0900

    react 컴포넌트의 import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import { value1, value2, value3 as v3, value4 } from "./MyValues.jsx";
-import { myAddress, myStr, value3 } from "./MyComps.jsx";
-
-// import : 다른 파일에 있는 변수를 현재 파일에서 사용
+import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
+import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
+  action1();
+  action2();
+  action3();
+
   return (
     <div>
-      <div>value1 : {value1}</div>
-      <div>value2 : {value2}</div>
-      <div>value3 : {v3}</div>
-      <div>value4 : {value4.team}</div>
-      <div>myStr : {myStr}</div>
-      <div>myAddress : {myAddress}</div>
-      <div>value3 : {value3}</div>
+      <YourComp />
+      <HerComp />
     </div>
   );
 }
