e0c8df0ac83e234155706017aa6c4408ae7b542f
kyb <kyb-0215@naver.com>
2024-05-13 11:43:57 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-  let val2 = 0;
-  console.log("다시 호출됨");
-
-  return (
-    <div>
-      <button onClick={() => setVal1(val1 + 1)}>호출 횟수 {val1}</button>
-      <button onClick={() => val2++}>호출 횟수 {val2}</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
