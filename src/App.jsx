9086b81f89910bf62fc89b6d431d04fd962f3120
kyb <kyb-0215@naver.com>
2024-05-13 11:41:34 +0900

    상태 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val1, setVal1] = useState(0);
+  let val2 = 0;
+  console.log("다시 호출됨");
+
+  return (
+    <div>
+      <button onClick={() => setVal1(val1 + 1)}>호출 횟수 {val1}</button>
+      <button onClick={() => val2++}>호출 횟수 {val2}</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
