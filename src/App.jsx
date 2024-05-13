541190e2cd9cf97e3d31fc16e3fcd2865e355b40
kyb <kyb-0215@naver.com>
2024-05-13 09:35:48 +0900

    컴포넌트 다른 파일로 옮기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { MyComp } from "./MyComp.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <MyComp />
+      </div>
+    </div>
+  );
 }
 
 export default App;
