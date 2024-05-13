3c40cfc90df8e208913554181337155229bab76a
kyb <kyb-0215@naver.com>
2024-05-13 12:29:48 +0900

    react 변경 시점

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp1() {
+  const [val, setVal] = useState(0);
+
+  function updateVal() {
+    // react는 re-rendering 시점을 적절한 순간으로 선택
+    setVal(val + 1);
+    setVal(val + 1);
+    setVal(val + 1);
+    console.log("val", val);
+  }
+
+  return (
+    <div>
+      <div>{val}</div>
+      <div>
+        <button onClick={updateVal}>증가</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <MyComp1 />
+      </div>
+    </div>
+  );
 }
 
 export default App;
