e6095cb677014c5808375e9b2b5816253ed4819e
kyb <kyb-0215@naver.com>
2024-05-14 11:16:34 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react";
-
-function MyButton({ count, setCount }) {
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>Click {count}</button>
-    </div>
-  );
-}
-
-function MyBox({ count }) {
-  return <div>{count} 번 클릭됨</div>;
-}
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <MyButton count={count} setCount={setCount} />
-      <hr />
-      <MyBox count={count} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
