1ef51cd54b4bcd4ab8aa59411797104974befa87
kyb <kyb-0215@naver.com>
2024-05-14 15:10:08 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
-
-  useEffect(() => {
-    console.log("count가 변경 될 때");
-    // dependency가 변경되지 않도록 주의
-    // setCount(count + 1); 변경하는 함수 작성 불가능
-  }, [count]);
-
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
