3fde07eaf48162ec7d21f24efc3b13c60f13e33b
kyb <kyb-0215@naver.com>
2024-05-14 10:24:10 +0900

    lift state up 상태 유지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ count, setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>up</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [display, setDisplay] = useState(true);
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={display}
+        onChange={(e) => setDisplay(e.target.checked)}
+      />
+      {display && <MyBox count={count} setCount={setCount} />}
+    </div>
+  );
 }
 
 export default App;
