97c436821523e68501e4a1817461cc3e89c8e32f
kyb <kyb-0215@naver.com>
2024-05-14 11:26:54 +0900

    prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ message }) {
+  return <div>{message}</div>;
+}
+
+function MySection({ message }) {
+  return <MyBox message={message} />;
+}
+
+function MyContainer({ message }) {
+  return <MySection message={message} />;
+}
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+  return (
+    <div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <p>{message}</p>
+      <hr />
+
+      <MyContainer message={message} />
+    </div>
+  );
 }
 
 export default App;
