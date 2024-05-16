892a7bd97991b8730ab241d5d396bc6578f47ef0
kyb <kyb-0215@naver.com>
2024-05-16 10:41:50 +0900

    /api/someurl4의 get,post 요청

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,29 @@ function App(props) {
     axios.get(`/api/someurl3?${params}`);
   }
 
+  function handleClickButton6() {
+    const params = new URLSearchParams();
+    params.append("id", 5);
+    params.append("city", "seoul");
+    params.append("country", "한국");
+    axios.post("/api/someurl3", params);
+  }
+
+  function handleClickButton7() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "kim");
+    axios.get("/api/someurl4?" + params);
+  }
+
+  function handleClickButton8() {
+    const params = new URLSearchParams();
+    params.append("foods", "pizza");
+    params.append("foods", "coffee");
+    params.append("city", "busan");
+    axios.post("/api/someurl4", params);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
@@ -35,6 +58,9 @@ function App(props) {
       <button onClick={handleClickButton3}>get 요청2</button>
       <button onClick={handleClickButton4}>post 요청2</button>
       <button onClick={handleClickButton5}>get 요청 with query string</button>
+      <button onClick={handleClickButton6}>post 요청 with query string</button>
+      <button onClick={handleClickButton7}>get 요청 qs</button>
+      <button onClick={handleClickButton8}>post 요청 qs</button>
     </div>
   );
 }
