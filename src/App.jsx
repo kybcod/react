3b9ebdb3fd7ebe26a49b4290c73e0fc4034b4d42
kyb <kyb-0215@naver.com>
2024-05-16 11:11:22 +0900

    직렬화

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 function App(props) {
+  // 직렬화
+  // 수동
+  const obj1 = { name: "son", age: 33 };
+  const json1 = '{"name" :"son", "age":33}';
+
+  // 메소드 사용
+  const obj2 = { name: "lee", age: 22 };
+  const json2 = JSON.stringify(obj2); // 직렬화
+  console.log(obj2);
+  console.log(json2);
+
   return <div></div>;
 }
 
