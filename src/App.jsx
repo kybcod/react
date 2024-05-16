9bf14583ac0bf1956f4dcac415e8bcde0a838638
kyb <kyb-0215@naver.com>
2024-05-16 10:01:02 +0900

    /api/someurl의 get, post 요청 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,20 @@ function App(props) {
     axios.post("/api/someurl");
   }
 
+  function handleClickButton4() {
+    axios.post("/api/someurl2");
+  }
+
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
       <button onClick={handleClickButton2}>post 요청</button>
+      <button onClick={handleClickButton3}>get 요청2</button>
+      <button onClick={handleClickButton4}>post 요청2</button>
     </div>
   );
 }
