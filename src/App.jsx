f6be33525a50f3848981506805d26c03b937084d
kyb <kyb-0215@naver.com>
2024-05-16 09:21:54 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [customerId, setCustomerId] = useState("");
-  const [customerName, setCustomerName] = useState("");
-
-  function handleSearchClick() {
-    // 조회버튼 클릭 시 /api/customer?id=#로 요청
-    axios
-      .get(`/api/customer?id=${customerId}`)
-      .then((response) => response.data)
-      .then((data) => setCustomerName(data));
-  }
-
-  return (
-    <div>
-      <div>
-        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
-      </div>
-      <button onClick={handleSearchClick}>조회</button>
-      <div>이름 : {customerName}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
