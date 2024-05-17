d9052d5ca0f2d9bbc205e173f7cca7d224dc5b67
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-17 20:54:49 +0900

    err 서버 오류 처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,85 @@ import React from 'react';
 import axios from "axios";
 
 function App(props) {
-    function handleClick200() {
+    function handle200() {
         axios.get("/api/main43/sub1")
-            .then(response => {
-                console.log("200 응답");
+            .then((res)=> console.log(res))
+    }
+
+    function handle400() {
+        axios.get("/api/main43/sub2")
+            .then((res) => {
+            })
+            .catch((err) => {
+                console.log(err)
+                if (err.response.status === 400) {
+                    console.log("잘못된 요청입니다.")
+                } else if (err.response.status === 404) {
+                    console.log("찾을 수 없는 페이지입니다.")
+                } else if (err.response.status === 500) {
+                    console.log("서버에러입니다.")
+                }
             });
     }
 
-    function handleClick400() {
-        axios.get("/api/main43/sub2").
-        then((res)=> console.log("400 응답"))
-            .catch(()=> console.log("catch 400 응답"));
+    function handle500() {
+        axios
+            .get("/api/main43/sub6")
+            .then((res) => {})
+            .catch((err) => {
+                if (err.response.status === 400) {
+                    console.log("잘 못된 요청입니다.");
+                } else if (err.response.status === 404) {
+                    console.log("존재하지 않는 요청입니다.");
+                } else if (err.response.status === 500) {
+                    console.log("서버에서 오류가 발생하였습니다.");
+                }
+            });
     }
 
-    function handelClick401() {
-        axios.get("/api/main43/sub3")
-            .then((res)=> console.log("401 응답"))
-            .catch(()=> console.log("catch 401 응답"));
+    function handle999() {
+        const r = Math.random();
+        let path = "";
+        if (r < 1 / 3) {
+            path = "/api/main43/sub1";
+        } else if (r < 2 / 3) {
+            path = "/api/main43/sub3";
+        } else {
+            path = "/api/main43/sub4";
+        }
+        axios
+            .get(path)
+            .then(() => {
+                console.log("잘 됨 ");
+            })
+            .catch((error) => {
+                const code = error.response.status;
+
+                switch (code) {
+                    case 401:
+                        console.log("401 응답 입니다. 로그인되지 않았습니다.");
+                        break;
+                    case 403:
+                        console.log("403 응답 입니다. 권한이 없습니다.");
+                        break;
+                }
+            })
+            .finally(() => {
+                // finally : 응답 코드가 200이든 아니든 항상 실행됨
+                console.log("항상 실행되는 메소드");
+            });
     }
 
-    function handelClick403() {
-        axios.get("/api/main43/sub4")
-            .then((res)=> console.log("403 응답"))
-            .catch(()=> console.log("catch 403 응답"));
-    }
-
-    function handelClick404() {
-        axios.get("/api/main43/sub5")
-            .then((res)=> console.log("404 응답"))
-            .catch(()=> console.log("catch 404 응답"));
-    }
-
-    function handelClick500() {
-        axios.get("/api/main43/sub6")
-            .then((res)=> console.log("500 응답"))
-            .catch(()=> console.log("catch 500 응답"));
-    }
 
     return (
         <div>
-          <button onClick={handleClick200}>200 응답</button>
-          <button onClick={handleClick400}>400 응답</button>
-          <button onClick={handelClick401}>401 응답</button>
-          <button onClick={handelClick403}>403 응답</button>
-          <button onClick={handelClick404}>404 응답</button>
-          <button onClick={handelClick500}>500 응답</button>
+            <button onClick={handle200}>200 응답</button>
+            <button onClick={handle400}>400 응답</button>
+            <button onClick={handle500}>500 응답</button>
+
+            {/* 200, 401, 403 응답일 때 각각 다른 일하는 코드 작성 */}
+            <button>응답 코드에 따라 다른 일하기</button>
+            <button onClick={handle999}>응답 코드에 따라 다른 일하기</button>
         </div>
     );
 }
