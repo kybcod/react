5b966c24e4b1ab1172e8e4f08c56dbec9bc1fbdf
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-18 15:08:40 +0900

    서브 경로는 children 프로퍼티 작성 가능

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,37 @@ import {BoardList} from "./BoardList.jsx";
 
 // 경로 / 생략 가능
 const router = createBrowserRouter([{
-    path: "/add",
-    element: <BoardAdd/>,
-}, {
-    path: "/edit",
-    element: <BoardEdit/>,
-},{
-    path: "list",
-    element: <BoardList/>
-},{
-    path:"/about",
-    element:<div>해당 게시글 관련 페이지</div>
-},{
-    path:"/search",
-    element:<div>검색 페이지</div>},{
-},{
-    path:"/home",
-    element:<div>메인 페이지</div>
-}, {
-    path:"/board/write",
-    element:<div>게시글 작성하기</div>
-},{
-    path:"/board/edit",
-    element:<div>게시글 수정하기</div>
-}]);
+        path: "/add",
+        element: <BoardAdd/>,
+    }, {
+        path: "/edit",
+        element: <BoardEdit/>,
+    },{
+        path: "list",
+        element: <BoardList/>
+    },{
+        path:"/about",
+        element:<div>해당 게시글 관련 페이지</div>
+    },{
+        path:"/search",
+        element:<div>검색 페이지</div>},{
+    },{
+        path:"/home",
+        element:<div>메인 페이지</div>
+    }, {
+        path:"/board",
+        children:[
+            {
+                path: "write",
+                element: <div>게시글 작성하기</div>
+            },
+            {
+                path: "edit",
+                element: <div>게시글 수정하기</div>
+            }
+            ]
+    }
+]);
 
 
 function App(props) {
