53497bbe4079cf23dc3a38e6d7ce21281a0fb2fd
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-18 15:25:29 +0900

    Link 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Link, Outlet, RouterProvider} from "react-router-dom";
 
 function SpringRoot() {
     return <div>
-        <div style={{padding:"10px", backgroundColor:"#eee"}}>상단 네브바</div>
+        <div style={{padding:"10px", backgroundColor:"#eee", display:"flex", gap:"5px"}}>
+            <div><a href="/spring/learn">LEARN</a></div>
+            <div><a href="/spring/api">API</a></div>
+            <div><a href="/spring/doc">DOC</a></div>
+        </div>
+
+        <div style={{padding:"10px", backgroundColor:"#eee", display:"flex", gap:"5px"}}>
+            <div><Link to="/spring/learn">LEARN</Link></div>
+            <div><Link to="/spring/api">API</Link></div>
+            <div><Link to="/spring/doc">DOC</Link></div>
+        </div>
         <div style={{marginTop:"20px"}}>
             <Outlet/>
         </div>
