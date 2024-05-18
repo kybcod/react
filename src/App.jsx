3aa4677f6dc663b7d9400a2073e0ddd117c76c97
ybk <kyb-0215@naver.com>
2024-05-18 21:22:21 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,7 @@
-import React from 'react';
-import {createBrowserRouter, Link, Outlet, RouterProvider} from "react-router-dom";
-
-function Root() {
-    return (
-        <div>
-            <div>
-                <Link to="/">Main</Link>
-                <Link to="/list">List</Link>
-            </div>
-            <hr/>
-            <div><Outlet/></div>
-        </div>
-    );
-}
-
-function BoardList() {
-    return <div>
-        <div>1번 게시물 보기</div>
-        <div>2번 게시물 보기</div>
-        <div>3번 게시물 보기</div>
-    </div>;
-}
-
-const router = createBrowserRouter([{
-    path: "/",
-    element:<Root/>,
-    children:[{
-        index: true, element:<div>Main</div>
-    },{
-        path:"board",
-        element:<div>BOARD PAGE</div>
-    },{
-        path:"list",
-        element:<BoardList/>
-    }]
-}])
+import React from "react";
 
 function App(props) {
-    return <RouterProvider router={router} />
+  return <div></div>;
 }
 
 export default App;
