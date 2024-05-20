9024a8767a07e6efb546620ba4fe2b1d4e99f9f1
kyb <kyb-0215@naver.com>
2024-05-20 10:16:50 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      {/*Responsive Styles*/}
+      <Box bgColor={"orange"} w={"500px"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        Lorme ipsum dolor.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
