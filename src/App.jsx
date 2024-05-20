5515df5607f795f8f05c6517fd686d8c488c344d
kyb <kyb-0215@naver.com>
2024-05-20 10:06:33 +0900

    스페이스

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box m={1} bgColor={"tomato"}>
+        lorem3
+      </Box>
+      <Box m={2} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={10} bgColor={"tomato"}>
+        Doloribus illo, saepe.
+      </Box>
+      <Box m={20} bgColor={"tomato"}>
+        Aut, eos officia.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
