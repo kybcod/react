36705161e297738bc1b318011ac0cf0b146b6699
kyb <kyb-0215@naver.com>
2024-05-20 14:33:50 +0900

    기본 동작 막기

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
-import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Button,
-  ChakraProvider,
-  useDisclosure,
-  useToast,
-} from "@chakra-ui/react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {}, []);
-  const toast = useToast();
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  function handleLinkClick(e) {
+    // 기본 이벤트 동작 막기
+    e.preventDefault();
 
-  function handleButtonClick(e) {
-    //event bubbling 멈추기
-    e.stopPropagation();
-    console.log("버튼 클릭");
+    // 개발자가 원하는 동작 작성
   }
 
-  function handleBoxClick() {
-    console.log("박스 클릭");
+  function handleSubmitForm() {
+    e.preventDefault();
+    console.log("다른 동작");
   }
 
   return (
-    <ChakraProvider>
-      <Box
-        onClick={handleBoxClick}
-        w={"100px"}
-        h={"100px"}
-        bgColor={"orange.200"}
-      >
-        <Button onClick={handleButtonClick}>클릭</Button>
-      </Box>
-    </ChakraProvider>
+    <div>
+      <a href="https://www.naver.com" onClick={handleLinkClick}>
+        네이버
+      </a>
+      <hr />
+      <form action="https://www.naver.com" onSubmit={handleSubmitForm}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
   );
 }
 
