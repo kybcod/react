b90b3af9d54554954d6a3ba7d71ce928b228226a
kyb <kyb-0215@naver.com>
2024-05-20 09:47:06 +0900

    Box, 컬러, 마진, 패딩, 높이, 너비

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,77 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      {/*버튼의 색 적용*/}
-      <div>
-        <Button colorScheme={"gray"}>Gray</Button>
-        <Button colorScheme={"red"}>Lorem.</Button>
-        <Button colorScheme={"orange"}>Quaerat.</Button>
-        <Button colorScheme={"blue"}>Voluptatem.</Button>
-        <Button colorScheme={"yellow"}>Culpa!</Button>
-        <Button colorScheme={"green"}>Quaerat.</Button>
-        <Button colorScheme={"teal"}>Neque.</Button>
-        <Button colorScheme={"pink"}>Expedita?</Button>
-        <Button colorScheme={"cyan"}>Quam!</Button>
-        <Button colorScheme={"purple"}>Quia?</Button>
-        <hr />
-        {/*로딩 표시*/}
-        <Button colorScheme={"blue"} isLoading={true}>
-          Lorem.
-        </Button>
-        <hr />
-        {/* 버튼 다양한 스타일 : solid, outline, ghost, link*/}
-        <Button colorScheme={"blue"} variant={"solid"}>
-          Lorem.
-        </Button>
-        <Button colorScheme={"blue"} variant={"outline"}>
-          Modi.
-        </Button>
-        <Button colorScheme={"blue"} variant={"ghost"}>
-          Beatae.
-        </Button>
-        <Button colorScheme={"blue"} variant={"link"}>
-          Qui!
-        </Button>
-        <hr />
+      <div
+        style={{
+          padding: "10px",
+          margin: "20px",
+          border: "1px solid black",
+          backgroundColor: "tomato",
+        }}
+      >
+        Lorem ipsum dolor.
       </div>
+      <Box p={"10px"} m={"20px"} border={"1px solid black"} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      {/*color(색상)*/}
+      <Box bgColor={"orange.50"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"orange.100"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"orange.200"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"orange.300"}>Lorem ipsum dolor.</Box>
+      <hr />
+      {/*margin : m,mt,mb,ml,mr,mx,my*/}
+      <Box m={"10px"} bgColor={"skyblue"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box mt={"10px"} bgColor={"skyblue"}>
+        Dicta, magnam quaerat.
+      </Box>
+      <Box mb={"10px"} bgColor={"skyblue"}>
+        Debitis, sit tenetur.
+      </Box>
+      <Box ml={"10px"} bgColor={"skyblue"}>
+        Dolor, inventore, rerum?
+      </Box>
+      <Box mr={"10px"} bgColor={"skyblue"}>
+        Aut, exercitationem, quisquam!
+      </Box>
+      <Box mx={"10px"} bgColor={"skyblue"}>
+        Cumque molestiae, sint.
+      </Box>
+      <Box my={"10px"} bgColor={"skyblue"}>
+        Nisi, saepe, voluptatibus?
+      </Box>
+      <hr />
+      {/*padding : p, pt, pb, pl, pr, px, py*/}
+      <Box bgColor={"teal"} p={"10px"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"teal"} pt={"10px"}>
+        A placeat, repellendus?
+      </Box>
+      <Box bgColor={"teal"} pb={"10px"}>
+        Ab, eaque, voluptate!
+      </Box>
+      <Box bgColor={"teal"} pl={"10px"}>
+        Illo ipsa, officia.
+      </Box>
+      <Box bgColor={"teal"} pr={"10px"}>
+        Atque, dolorum, maxime?
+      </Box>
+      <Box bgColor={"teal"} px={"10px"}>
+        Maxime provident, quae!
+      </Box>
+      <Box bgColor={"teal"} py={"10px"}>
+        Eius, error temporibus.
+      </Box>
+      {/* w(넓이), h(높이) */}
+      <hr />
+      <Box w={"500px"}>Lorem ipsum dolor.</Box>
+      <Box h={"300px"}>Atque, mollitia quos.</Box>
     </ChakraProvider>
   );
 }
