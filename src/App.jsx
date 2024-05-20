ae0a23039f5930f38c098869aeab4d8b6fa8e285
kyb <kyb-0215@naver.com>
2024-05-20 10:25:16 +0900

    sx : 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
+        Lorem ipsum dolor.
+      </Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
