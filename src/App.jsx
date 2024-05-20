4228b2e4db0b4b80d03b6154a4cee61b8ee76dd7
kyb <kyb-0215@naver.com>
2024-05-20 14:19:44 +0900

    event bubbling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 
 function App(props) {
   const [count, setCount] = useState(0);
   useEffect(() => {}, []);
   const toast = useToast();
   const { isOpen, onClose, onOpen } = useDisclosure();
+
+  function handleButtonClick(e) {
+    //event bubbling 멈추기
+    e.stopPropagation();
+    console.log("버튼 클릭");
+  }
+
+  function handleBoxClick() {
+    console.log("박스 클릭");
+  }
+
   return (
     <ChakraProvider>
-      <div></div>
+      <Box
+        onClick={handleBoxClick}
+        w={"100px"}
+        h={"100px"}
+        bgColor={"orange.200"}
+      >
+        <Button onClick={handleButtonClick}>클릭</Button>
+      </Box>
     </ChakraProvider>
   );
 }
