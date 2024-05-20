88811cd33725a27f3eb1f37e72dd216a834d317f
kyb <kyb-0215@naver.com>
2024-05-20 10:26:28 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
-        Lorem ipsum dolor.
-      </Button>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
