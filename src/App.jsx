8e09c22fd4910d73931afd8381c12abc34b60ede
kyb <kyb-0215@naver.com>
2024-05-20 11:12:51 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
 import React from "react";
-import {
-  Box,
-  Center,
-  ChakraProvider,
-  Checkbox,
-  FormControl,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-  Switch,
-  Textarea,
-} from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <label htmlFor="input1">이름</label>
-      <input id={"input1"} type="text" />
-      <hr />
-      <Center>
-        <Box w={{ base: "100%", md: "50%" }}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-          </FormControl>
-        </Box>
-      </Center>
-      <hr />
-      <Checkbox>lorem1</Checkbox>
-      <Checkbox>Lorem.</Checkbox>
-      <Checkbox>Officiis.</Checkbox>
-      <Checkbox>Alias?</Checkbox>
-      <hr />
-      <Switch />
-      <hr />
-      <RadioGroup>
-        <Radio value={"1"}>1</Radio>
-        <Radio value={"2"}>2</Radio>
-        <Radio value={"3"}>3</Radio>
-      </RadioGroup>
-      <hr />
-      <Textarea></Textarea>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
+
 export default App;
