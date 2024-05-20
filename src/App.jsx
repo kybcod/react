ec37b19060e145f506008eb35bbcab2cea220cc8
kyb <kyb-0215@naver.com>
2024-05-20 15:11:06 +0900

    file 전송

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    // 기본 이벤트 동작 막기
-    e.preventDefault();
-
-    // 개발자가 원하는 동작 작성
-  }
-
-  function handleSubmitForm() {
-    e.preventDefault();
-    console.log("다른 동작");
-  }
-
   return (
     <div>
-      <a href="https://www.naver.com" onClick={handleLinkClick}>
-        네이버
-      </a>
-      <hr />
-      <form action="https://www.naver.com" onSubmit={handleSubmitForm}>
-        <input type="text" />
-        <button>전송</button>
+      <form action="/path" method="POST" encType="multipart/form-data">
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name="file" />
+        <br />
+        <input type="submit" />
       </form>
     </div>
   );
