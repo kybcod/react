f3f27e664d7d02452aed1061bd42aad109f527ec
kyb <kyb-0215@naver.com>
2024-05-20 10:07:39 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Box m={1} bgColor={"tomato"}>
-        lorem3
-      </Box>
-      <Box m={2} bgColor={"tomato"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box m={10} bgColor={"tomato"}>
-        Doloribus illo, saepe.
-      </Box>
-      <Box m={20} bgColor={"tomato"}>
-        Aut, eos officia.
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
